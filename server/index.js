a2989fd95127110c38aa6a051992aaa632663e3b
Nina Tan <ninavtan@gmail.com>
2022-03-06 23:28:51 -0500

    Basic passport functionality is working
    Now -> hook up client side

--- server/index.js
+++ server/index.js
@@ -72,8 +72,8 @@ const port = process.env.PORT || 3001;
 // );
 app.use(express.json({limit: '25mb'}));
 app.use(express.urlencoded({limit: '25mb', extended: true}));
-app.use(mainRoutes);
-app.use('/', authRoutes);
+// app.use(mainRoutes);
+app.use(authRoutes);
 
 
 app.get("/api", (req, res) => {
