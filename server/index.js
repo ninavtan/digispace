67b76aafbea3a35716c970389925e9e01414edd6
Nina Tan <ninavtan@gmail.com>
2022-03-15 21:33:51 -0400

    heroku deploy.....

--- server/index.js
+++ server/index.js
@@ -96,5 +96,12 @@ app.get("/api", (req, res) => {
   res.json({message: "Hello from Express!"});
 });
 
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
