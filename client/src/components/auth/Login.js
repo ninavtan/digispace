2b895a85ac97a428cf0570eb6f352776fbee5c5c
Nina Tan <ninavtan@gmail.com>
2022-04-05 18:46:27 -0400

    Fixing heroku

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -3,16 +3,9 @@ import { useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { loginUser } from "../../redux/actions";
 import styled from 'styled-components';
-import { AuthContext } from "../../App";
 
 export default function Login() {
-  // const navigate = useNavigate();
-  const authContext = useContext(AuthContext);
 
-  const { auth, setAuth } = useContext(AuthContext);
-
-
-  // const currentUser = useSelector(state => state.auth.user);
   const dispatch = useDispatch();
 
   function handleLogin(e) {
@@ -22,23 +15,10 @@ export default function Login() {
     dispatch(loginUser(form[0].value, form[1].value))
       .then((err, result) => {
         if (err) console.log(err)
-        setAuth(true);
+     
         // navigate("/home");
       })
-   
-    // dispatch(loginUser(form[0].value, form[1].value));
-  }
 
-
-  // useEffect(() => {
-  //   console.log(currentUser);
-  //   if (currentUser.username !== null) {
-  //     navigate("/home")
-  //   } else {
-  //     console.log('Wrong password');
-  //   }
-  // })
-  
   return (
     <LargeSquareContainer>
     <FormDiv>
@@ -52,20 +32,18 @@ export default function Login() {
     </LargeSquareContainer>
   )
 }
+}
 
 const FormDiv = styled.div`
-  // text-align: center;
   margin-top: 20%;
   padding: 20%;
-
 `
 
 const LargeSquareContainer = styled.div`
   height: 500px;
   width: 500px;
   background-color: #DAEDBD;
- margin: 2em auto;
- z-index: 3;
-
+  margin: 2em auto;
+  z-index: 3;
 `
 
